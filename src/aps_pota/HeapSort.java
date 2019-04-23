1529881506f314160c1d116fdfee00f258baee79
RzorX <49616719+RzorX@users.noreply.github.com>
2019-04-23 19:52:01 -0300

    HeapSort

--- src/aps_pota/HeapSort.java
+++ src/aps_pota/HeapSort.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aps_pota;
 
-/**
- *
- * @author lab651
- */
 public class HeapSort extends AlgOrd {
 
     public HeapSort(String nome) {
