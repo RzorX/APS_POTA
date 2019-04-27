65fb844dfdd1fb183caefd1ddf3ba001f52069bd
RzorX <49616719+RzorX@users.noreply.github.com>
2019-04-27 14:35:35 -0300

    Add Bucket

---
+++ src/aps_pota/BucketSort.java
@@ -0,0 +1,15 @@
+package aps_pota;
+
+public class BucketSort extends AlgOrd {
+
+    public BucketSort(String nome) {
+        super(nome);
+    }
+
+    @Override
+    public void Orderna() {
+        //bucketSort(vetor);
+        this.tamanho = vetor.length;
+    }
+
+}
